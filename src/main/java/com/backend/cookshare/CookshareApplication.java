72ed6e0775c363af9a8802bb855da0c749ea9058
nqluong <nguyenluong052004@gmail.com>
2025-09-29 00:08:49 +0700

    init project

---
+++ src/main/java/com/backend/cookshare/CookshareApplication.java
@@ -0,0 +1,13 @@
+package com.backend.cookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/cookshare/CookshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.cookshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
