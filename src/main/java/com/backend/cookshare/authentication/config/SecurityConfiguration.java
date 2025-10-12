f27efef301543b55e43f248a2a30cdc370f97717
nqluong <nguyenluong052004@gmail.com>
2025-10-13 01:09:30 +0700

    Update main

--- src/main/java/com/backend/cookshare/authentication/config/SecurityConfiguration.java
+++ src/main/java/com/backend/cookshare/authentication/config/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults()) // Enable CORS
                 .authorizeHttpRequests(
                         authz -> authz
-                                .requestMatchers("/", "/login", "/register").permitAll()
+                                .requestMatchers("/", "/login", "/register", "/api/recipes/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
