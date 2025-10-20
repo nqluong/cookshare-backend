90a388d4190062c28789db8b1786f001a05a5c01
bachpanh <bpanh044@gmail.com>
2025-10-20 17:12:39 +0700

    Cap nhap tao cong thuc

--- src/main/java/com/backend/cookshare/common/exception/ErrorCode.java
+++ src/main/java/com/backend/cookshare/common/exception/ErrorCode.java
@@ -12,6 +12,8 @@ public enum ErrorCode {
     RECIPE_NOT_FOUND(1001, "Không tìm thấy công thức", HttpStatus.NOT_FOUND),
     VALIDATION_ERROR(1002, "Dữ liệu không hợp lệ", HttpStatus.BAD_REQUEST),
     BAD_REQUEST(1003, "Yêu cầu không hợp lệ", HttpStatus.BAD_REQUEST),
+    METHOD_NOT_ALLOWED(1004, "Phương thức HTTP không được hỗ trợ", HttpStatus.METHOD_NOT_ALLOWED),
+    NOT_FOUND(1005, "Không tìm thấy dữ liệu yêu cầu", HttpStatus.NOT_FOUND),
 
     CANNOT_FOLLOW_YOURSELF(2001, "Không thể follow chính mình", HttpStatus.BAD_REQUEST),
     ALREADY_FOLLOWING(2002, "Bạn đã follow người dùng này rồi", HttpStatus.BAD_REQUEST),
