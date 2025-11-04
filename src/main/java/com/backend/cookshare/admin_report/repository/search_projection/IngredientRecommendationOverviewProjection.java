754a7378c26ad9d87a82186f23784633dd84b42f
nqluong <nguyenluong052004@gmail.com>
2025-11-05 00:18:55 +0700

    delete search recommendation

--- src/main/java/com/backend/cookshare/admin_report/repository/search_projection/IngredientRecommendationOverviewProjection.java
+++
@@ -1,6 +0,0 @@
-package com.backend.cookshare.admin_report.repository.search_projection;
-
-public interface IngredientRecommendationOverviewProjection {
-    Long getTotalSearches();
-    Long getTotalRecommendations();
-}
--- src/main/java/com/backend/cookshare/admin_report/repository/search_projection/RecommendationStatsProjection.java
+++
@@ -1,9 +0,0 @@
-package com.backend.cookshare.admin_report.repository.search_projection;
-
-import java.math.BigDecimal;
-
-public interface RecommendationStatsProjection {
-    Long getShownCount();
-    Long getClickedCount();
-    BigDecimal getAvgPosition();
-}
